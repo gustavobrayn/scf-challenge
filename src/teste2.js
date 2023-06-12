8e4acb415bfedf0c294ca780e84a99b3de99b76a
Gustavo Brayn <gustavo.augusto.brayn@gmail.com>
2023-06-12 11:42:36 -0300

    feat(teste2): add body params validation before create a new user

--- src/teste2.js
+++ src/teste2.js
@@ -3,6 +3,10 @@ const data = require('./fakeData')
 module.exports = function (req, res) {
   const { name, job } = req.body
 
+  if (!name || !job) {
+    return res.status(400).send('Não foi possível criar um novo usuário.')
+  }
+
   const newUser = {
     name,
     job,
