4851eb07ef8999bca2257c0b31ef0cb266aa7c28
Gustavo Brayn <gustavo.augusto.brayn@gmail.com>
2023-06-12 12:21:52 -0300

    feat(teste3): add user not found validation

--- src/teste3.js
+++ src/teste3.js
@@ -4,8 +4,13 @@ module.exports = function (req, res) {
   const { name } = req.query
 
   const userIndex = data.findIndex((user) => user.name === name)
+  const userNotFound = userIndex === -1
+
+  if (userNotFound) {
+    return res.status(404).send('Usuário não encontrado.')
+  }
 
   data.splice(userIndex, 1)
 
-  res.send('success')
+  return res.send('success')
 }
