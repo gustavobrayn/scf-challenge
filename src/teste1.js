b8058f854565b2949a34b955090afa43a16537c2
Gustavo Brayn <gustavo.augusto.brayn@gmail.com>
2023-06-12 11:14:17 -0300

    refactor(teste1): increase readability

--- src/teste1.js
+++ src/teste1.js
@@ -1,24 +1,18 @@
-var data =  require("./fakeData");
+const data = require('./fakeData')
 
-const getUser = ( req, res, next ) => {
-    
-    var name =  req.query.name;
+const getUser = (req, res) => {
+  const { name } = req.query
 
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            res.send(data[i]);
-        }
-    }
+  const user = data.find((user) => user.name === name)
 
-};
+  return res.send(user)
+}
 
-const getUsers = ( req, res, next ) => {
-    
-    res.send(data);
-    
-};
+const getUsers = (req, res) => {
+  return res.send(data)
+}
 
 module.exports = {
-    getUser,
-    getUsers
-};
+  getUser,
+  getUsers,
+}
